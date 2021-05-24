f6f1a84e47fe5f0596ee60bbdaaeb6d083495818
Zenish Shrestha <zenishshr@gmail.com>
2021-05-24 10:01:21 +0545

    initial commit

--- src/components/AddToCard.js
+++ src/components/AddToCard.js
@@ -1,59 +1,58 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
 import {
-    Card,
-    CardActions,
-    CardContent,
-    IconButton,
-    Typography
-  } from "@material-ui/core";
-  import { Done, DeleteOutlined, UndoOutlined } from "@material-ui/icons";
-  import { markAsCompleted, deleteTodo } from "../reduxfeatures/TodoSlice";
-export default function AddToCard(id, status, taskName) {
-    const dispatch = useDispatch();
+  Card,
+  CardActions,
+  CardContent,
+  IconButton,
+  Typography
+} from "@material-ui/core";
+import { DeleteOutlined, Done, UndoOutlined } from "@material-ui/icons";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { deleteTodo, markAsCompleted } from "../reduxfeatures/TodoSlice";
+export default function AddToCard({ id, status, taskName }) {
+  const dispatch = useDispatch();
 
-    const handleDoneClick = () => {
-        dispatch(markAsCompleted(id));
-      };
-    
-      const handleDeleteClick = () => {
-        dispatch(deleteTodo(id));
-      };
-    
+  const handleDoneClick = () => {
+    dispatch(markAsCompleted(id));
+  };
 
-    return (
-        <Card style={{ width: "fit-content", margin: "20px 20px 0 0" }}>
-            <CardContent>
-            <Typography variant="body1">{taskName}</Typography>
-            <Typography variant="subtitle1">{status}</Typography>
-            </CardContent> 
-            <CardActions>
-            {/* Done Button */}
-            {/* Pending -> Completed */}
-            {/* tasks.map */}
-            <IconButton
-            onClick={handleDoneClick}
-            style={{
-                backgroundColor: status === "completed" ? "black" : "teal",
-                color: "white"
-            }}
-            >
-            {/* Conditional Rendering */}
-            {/* To display separate icons for different task status */}
-            {status === "completed" ? <UndoOutlined /> : <Done />}
-            </IconButton>
+  const handleDeleteClick = () => {
+    dispatch(deleteTodo(id));
+  };
 
-            {/* Delete Button */}
-            {/* Task should be removed -> tasks.filter */}
-            <IconButton
-            onClick={handleDeleteClick}
-            style={{
-                backgroundColor: "#eee"
-            }}
-            >
-            <DeleteOutlined />
-            </IconButton>
-            </CardActions>
-        </Card>
-    )
+  return (
+    <Card style={{ width: "fit-content", margin: "20px 20px 0 0" }}>
+      <CardContent>
+        <Typography variant="body1">{taskName}</Typography>
+        <Typography variant="subtitle1">{status}</Typography>
+      </CardContent>
+      <CardActions>
+        {/* Done Button */}
+        {/* Pending -> Completed */}
+        {/* tasks.map */}
+        <IconButton
+          onClick={handleDoneClick}
+          style={{
+            backgroundColor: status === "completed" ? "black" : "teal",
+            color: "white"
+          }}
+        >
+          {/* Conditional Rendering */}
+          {/* To display separate icons for different task status */}
+          {status === "completed" ? <UndoOutlined /> : <Done />}
+        </IconButton>
+
+        {/* Delete Button */}
+        {/* Task should be removed -> tasks.filter */}
+        <IconButton
+          onClick={handleDeleteClick}
+          style={{
+            backgroundColor: "#eee"
+          }}
+        >
+          <DeleteOutlined />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
 }
--- src/components/addtodo/Index.js
+++ src/components/addtodo/Index.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from "react";
 import CloseIcon from "@material-ui/icons/Close";
 import { Button, Snackbar, IconButton } from "@material-ui/core";
-import {useDispatch, useSelector} from "react-redux";
-import {  addTodo, closeSnackbar } from '../../reduxfeatures/TodoSlice';
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo, closeSnackbar } from "../../reduxfeatures/TodoSlice";
 export default function Index() {
-
-  const {snackbar} =useSelector((state)=>state.todo);
+  const { snackbar } = useSelector((state) => state.todo);
 
   const dispatch = useDispatch();
-  
-    return (
-        <>
-         <Button variant="contained" color="primary" onClick={()=>{
-          dispatch(addTodo());
+
+  return (
+    <>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          try {
+            console.log("Adding");
+            dispatch(addTodo());
+          } catch (e) {
+            console.log(e);
+          }
         }}
       >
         Add Todo
@@ -22,10 +29,9 @@ export default function Index() {
         open={snackbar.open}
         message={snackbar.message}
         autoHideDuration={1000}
-        onClose={()=>
-          dispatch(closeSnackbar())}
-          action={
-            <IconButton
+        onClose={() => dispatch(closeSnackbar())}
+        action={
+          <IconButton
             style={{ color: "white" }}
             onClick={() => {
               dispatch(closeSnackbar());
@@ -33,8 +39,8 @@ export default function Index() {
           >
             <CloseIcon />
           </IconButton>
-          }
-          />   
-        </>
-    );
+        }
+      />
+    </>
+  );
 }
